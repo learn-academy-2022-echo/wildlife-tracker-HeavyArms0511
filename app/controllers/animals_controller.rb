1a5233566a58dc2b47cefbd6a110e1581c3c32d8
HeavyArms0511 <learnacademy@LEARNs-MacBook-Air.local>
2022-09-10 21:00:33 -0700

    I got it working I believe

--- app/controllers/animals_controller.rb
+++ app/controllers/animals_controller.rb
@@ -1,23 +1,15 @@
 class AnimalsController < ApplicationController
-    
-    
-    def render_unprocessable_entity_response(exception)
-        render json: exception.record.errors, status: :unprocessable_entity
-      end
-
-      def render_not_found_response(exception)
-        render json: { error: exception.message }, status: :not_found
-      end
-
 
     def index
         animals = Animal.all
         render json: animals
     end
+
     def show
         animal = Animal.find(params[:id])
         render json: animal, include:[:sightings]
     end
+
     def create
         animal = Animal.create(animal_params)
         if animal.valid?
@@ -26,15 +18,12 @@ class AnimalsController < ApplicationController
             render json: animal.errors
         end
     end
+
     def update
         animal = Animal.find(params[:id])
         animal.update!(animal_params)
-        if animal.valid?
-            render json: animal
-        else 
-            render json: animal.errors
-        end
     end
+
     def destroy
         animal = Animal.find(params[:id])
         if animal.destroy
